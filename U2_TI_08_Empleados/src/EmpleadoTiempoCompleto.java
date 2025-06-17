de439a8db5674ccb154d2ac4845245f131922bab
oumandy2106 <20243ds039@utez.edu.mx>
2025-06-17 07:59:31 -0600

    Añadi los demas archivos y termine EmpleadoTiempoCompleto

--- U2_TI_08_Empleados/src/EmpleadoTiempoCompleto.java
+++ U2_TI_08_Empleados/src/EmpleadoTiempoCompleto.java
@@ -1,3 +1,14 @@
-public class EmpleadoTiempoCompleto {
+public class EmpleadoTiempoCompleto extends Empleado {
+    private String beneficios;
+    public EmpleadoTiempoCompleto(String id, String nombre, double salarioBase, String beneficios) {
+        super(id, nombre, salarioBase);
+        this.beneficios = beneficios;
+    }
+
+    @Override
+    public double calcularSalario() {
+        getSalarioBase();
+        return getSalarioBase();
+    }
 
 }
